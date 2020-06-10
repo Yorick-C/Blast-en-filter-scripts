277850c7de8423feb3efccf83597ac7c7a24b13d
Yorick-C <55192235+Yorick-C@users.noreply.github.com>
2020-06-10 16:26:57 +0200

    Update SQL_con.py

--- SQL_con.py
+++ SQL_con.py
@@ -3,21 +3,31 @@ def main():
 
 
 def filter():
-    with open('result35-80.txt', 'r') as file:
+    """
+    Een functie die door een gegeven txt file loopt die verkregen is 
+    van de filter.py en geeft alles weer wat er in zit in lists
+    :return: visualisatie van de data in een txt file
+     afkomstig van de filter.py
+    """
+    with open('een gegeven txt bestand', 'r') as file:
+        # maakt allemaal lists aan zodat ze later gebruikt kunnen worden
         read_lijst = []
         eiwit_lijst = []
         taxonomy_lijst = []
         accesie_lijst = []
         e_value_lijst = []
         score_lijst = []
+        # looped over de lines in de file en split alles
         for line in file:
             line = line.split('\t')
+            # zet erbij of het een forward of reverse read is
             if int(line[0]) % 2 > 0:
                 read = 'FW'
             elif int(line[0]) % 2 == 0:
                 read = 'RV'
             else:
                 print(line[0])
+            # wgeeft alle waardes hun data doormiddel van de juiste index eraan koppelen
             line[2] = line[2].replace(']', '')
             eiwit = line[2].split('[')
             taxonomy = eiwit[1]
@@ -25,6 +35,7 @@ def filter():
             accesie = line[3]
             e_value = line[4]
             score = line[5]
+            # zet alles in de lists
             read_lijst.append(read)
             eiwit_lijst.append(eiwit)
             taxonomy_lijst.append(taxonomy)
@@ -32,7 +43,7 @@ def filter():
             e_value_lijst.append(e_value)
             score = score.replace('\n', '')
             score_lijst.append(score)
-
+        # print de lists uit voor visualisatie van de data
         print(read_lijst)
         print(eiwit_lijst)
         print(taxonomy_lijst)
